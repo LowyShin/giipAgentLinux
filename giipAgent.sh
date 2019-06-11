77155b36a053d1955b0904791d3e0822bb83764f
Lowy Shin <lowyshin@littleworld.net>
2019-06-11 14:14:42 +0900

    Update giipAgent.sh

--- giipAgent.sh
+++ giipAgent.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# giipAgent Ver. 1.3
+# giipAgent Ver. 1.4
 # Written by Lowy Shin at 20140922
+# Supported OS : MacOS, CentOS, Ubuntu, Some Linux
 # 190611 Lowy, Change agent download method to git repository.
 
 # Usable giip variables =========
@@ -9,42 +10,60 @@
 # User Variables ===============================================
 . giipAgent.cnf
 
-# Check os2unix
+# Check dos2unix
 CHECK_Converter=`which dos2unix`
 RESULT=`echo $?`
 
 #OS Check
-ostype=`head -n 1 /etc/issue | awk '{print $1}'`
-if [ $ostype = "Ubuntu" ];then
-	os=`lsb_release -d`
-	os=`echo "$os"| sed -e "s/Description\://g"`
-
+# Check MacOS
+uname=`uname -a | awk '{print $1}'`
+if [ $uname = "Darwin" ];then
+	osname=`sw_vers -productName`
+	osver=`sw_vers -productVersion`
+	os="${osname} ${osver}"
+	os=`echo "$os" | sed -e "s/ /%20/g"`
 	if [ ${RESULT} != 0 ];then
-		apt-get install -y dos2unix
+		brew install dos2unix
 	fi
 else
-	os=`cat /etc/redhat-release`
+	ostype=`head -n 1 /etc/issue | awk '{print $1}'`
+	if [ $ostype = "Ubuntu" ];then
+		os=`lsb_release -d`
+		os=`echo "$os"| sed -e "s/Description\://g"`
 
-	if [ ${RESULT} != 0 ];then
-		yum install -y dos2unix
+		if [ ${RESULT} != 0 ];then
+			apt-get install -y dos2unix
+		fi
+	else
+		os=`cat /etc/redhat-release`
+
+		if [ ${RESULT} != 0 ];then
+			yum install -y dos2unix
+		fi
 	fi
 fi
 
 tmpFileName="giipTmpScript.sh"
 logdt=`date '+%Y/%m/%d %H:%M:%S'`
 Today=`date '+%Y%m%d'`
-LogFileName="/var/log/giipAgent_$Today.log"
+LogFileName="giipAgent_$Today.log"
+lwDownloadURL="http://giipapi.littleworld.net/api/cqe/queue/get03?sk=$sk&lssn=$lssn&os=$os&df=os"
+#echo $lwDownloadURL
 
-wget "http://giipapi.littleworld.net/api/cqe/queue/get03?sk=$sk&lssn=$lssn&os=$os&df=os" -O $tmpFileName
+curl -o $tmpFileName $lwDownloadURL
 
-echo "[$logdt]" >> $LogFileName
-
-ls -l $tmpFileName
-dos2unix $tmpFileName
+if [[ -s $tmpFileName ]];then
+	ls -l $tmpFileName
+	dos2unix $tmpFileName
+	echo "[$logdt] Downloaded queue... " >> $LogFileName
+else
+	echo "[$logdt] No queue" >> $LogFileName
+fi
 
 while [ -s $tmpFileName ];
 do
 
+	cmpFile=`cat $tmpFileName`
 	n=`sed -n '/\/expect/=' giipTmpScript.sh`
 	if [[ n -eq 1 ]]; then
 		expect ./giipTmpScript.sh >> $LogFileName
@@ -54,10 +73,24 @@ do
 		echo "Executed script..." >> $LogFileName
 	fi
 
-	wget "http://giipapi.littleworld.net/api/cqe/queue/get03?sk=$sk&lssn=$lssn&os=$os&df=os" -O $tmpFileName
-	ls -l $tmpFileName
-	dos2unix $tmpFileName
+	orgFile=`cat $tmpFileName`
+
+	curl -o $tmpFileName $lwDownloadURL
+
+	if [[ -s $tmpFileName ]];then
+		ls -l $tmpFileName
+		dos2unix $tmpFileName
+		echo "[$logdt] Downloaded queue..... " >> $LogFileName
+	else
+		echo "[$logdt] Process done..." >> $LogFileName
+	fi
+
+	if [[ $cmpFile = $orgFile ]];then
+		rm -f $tmpFileName
+	fi
+
 done
 
 rm -f $tmpFileName
+
 
